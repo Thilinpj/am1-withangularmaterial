fda456a9a6b070609dc3defde5c48a6c80a6adee
TiLiNi <thilinipramodalm@gmail.com>
2018-09-03 00:11:41 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import {MatButtonModule, MatCheckboxModule,MatToolbarModule,MatDividerModule, MatListModule} from '@angular/material';
+import { HomeComponent } from './components/home/home.component';
+import { FitnessInstitutesComponent } from './components/fitness-institutes/fitness-institutes.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'FitnessInstitutes', component: FitnessInstitutesComponent },
+  { path: 'Contact', component: ContactComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FitnessInstitutesComponent,
+    ContactComponent,
+   
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatDividerModule,
+    MatListModule,
+    RouterModule.forRoot([
+      {
+        path:'components/home',
+        component:HomeComponent
+      },
+      {
+        path:'components/fitnessinstitutes',
+        component:FitnessInstitutesComponent
+      },
+      
+      {
+        path:'components/contact',
+        component:ContactComponent
+      },
+      
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
